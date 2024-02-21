0fe8c8e512fe41525f2a44dd0a378bd2fc30d6f7
Himanshu Jain <himanshuain5567@gmail.com>
2024-02-21 20:11:39 +0530

    Searchbar css touchup

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -9,7 +9,7 @@ interface SearchBarProps {
 export const SearchBar = ({ setInput }: SearchBarProps) => {
   const [searchInput, setSearchInput] = useState("");
   return (
-    <div className="sticky top-0 z-10 bg-white py-4 px-6 flex items-center space-x-2 shadow">
+    <div className="sticky top-0 z-10 bg-white py-4 px-60 flex items-center space-x-2 shadow">
       <Input type="text" placeholder="Search" onChange={e => setSearchInput(e.target.value)} />
       <Button onClick={() => setInput(searchInput)}>
         <Search />
